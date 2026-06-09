860182e534493f7d08aaa9129ab043be347cd9d8
Allen Gao <144358603+A1fcodes@users.noreply.github.com>
2026-06-09 13:03:03 -0400

    Add files via upload

---
+++ external/sdk13/externel/micro-ecc/nrf52_keil/armgcc/_build/micro_ecc_lib_uECC.c.d
@@ -0,0 +1,41 @@
+_build/micro_ecc_lib_uECC.c.o: ../../micro-ecc/uECC.c \
+ ../../micro-ecc/uECC.h \
+ /Users/allengao/Downloads/gcc-arm-none-eabi-4_8-2013q4/lib/gcc/arm-none-eabi/4.8.3/include/stdint.h \
+ /Users/allengao/Downloads/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/stdint.h \
+ /Users/allengao/Downloads/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/_ansi.h \
+ /Users/allengao/Downloads/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/newlib.h \
+ /Users/allengao/Downloads/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/sys/config.h \
+ /Users/allengao/Downloads/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/machine/ieeefp.h \
+ /Users/allengao/Downloads/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/sys/features.h \
+ ../../micro-ecc/uECC_vli.h ../../micro-ecc/types.h \
+ ../../micro-ecc/platform-specific.inc ../../micro-ecc/asm_arm.inc \
+ ../../micro-ecc/asm_arm_mult_square_umaal.inc \
+ ../../micro-ecc/curve-specific.inc
+
+../../micro-ecc/uECC.h:
+
+/Users/allengao/Downloads/gcc-arm-none-eabi-4_8-2013q4/lib/gcc/arm-none-eabi/4.8.3/include/stdint.h:
+
+/Users/allengao/Downloads/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/stdint.h:
+
+/Users/allengao/Downloads/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/_ansi.h:
+
+/Users/allengao/Downloads/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/newlib.h:
+
+/Users/allengao/Downloads/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/sys/config.h:
+
+/Users/allengao/Downloads/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/machine/ieeefp.h:
+
+/Users/allengao/Downloads/gcc-arm-none-eabi-4_8-2013q4/arm-none-eabi/include/sys/features.h:
+
+../../micro-ecc/uECC_vli.h:
+
+../../micro-ecc/types.h:
+
+../../micro-ecc/platform-specific.inc:
+
+../../micro-ecc/asm_arm.inc:
+
+../../micro-ecc/asm_arm_mult_square_umaal.inc:
+
+../../micro-ecc/curve-specific.inc:
